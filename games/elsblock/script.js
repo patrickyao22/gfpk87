11529349eb90b027d1d9bdd5abfe15e186d317bb
Patrickyao <104066866+patrickyao22@users.noreply.github.com>
2023-12-11 20:45:21 +0800

    Update script.js

--- games/elsblock/script.js
+++ games/elsblock/script.js
@@ -122,12 +122,13 @@ var canvas = document.getElementById("tetris");
    break;
   }
   }
-  dc++;
-  document.getElementById("destory_line").innerHTML = dc;
+  
   if (flag) {
   clearRowList.push(i); //记录消除行号的索引
   
   }
+  dc++;
+  document.getElementById("destory_line").innerHTML = dc;
  }
  var clearRows = clearRowList.length;
  //所谓的消行就是将待消除行的索引，下方所有的格子上移动
